d0047deeb3aff7c0e7fbc7f9a2d2adaa2e9d6fad
Daniel Kukula <daniel.kuku@gmail.com>
2024-12-16 19:47:56 +0100

    add implemetation

--- mix.exs
+++ mix.exs
@@ -7,22 +7,32 @@ defmodule OpentelemetrySqlcommenter.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description:
+        "OpenTelemetry integration for SQLCommenter and Ecto, adding telemetry context to SQL queries",
+      package: [
+        licenses: ["Apache-2.0"],
+        links: %{
+          "GitHub" => "https://github.com/dkuku/opentelemetry_sqlcommenter",
+          "OpenTelemetry" => "https://opentelemetry.io",
+          "SQLCommenter" => "https://google.github.io/sqlcommenter/"
+        }
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:opentelemetry_exporter, "~> 1.8"},
+      {:opentelemetry, "~> 1.0"},
+      {:postgrex, "~> 0.19.3"},
+      {:ex_doc, "~> 0.35"}
     ]
   end
 end
